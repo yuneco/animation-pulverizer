ec17dd34ac31178aedce67d6f3dfc89084a1cf93
yuneco <yukiyuneco@gmail.com>
2021-12-10 16:10:51 +0900

    Chrome機能拡張の雛形作成

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { chromeExtension } from "vite-plugin-chrome-extension";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: "src/manifest.json",
+    },
+  },
+  plugins: [chromeExtension()],
+});
