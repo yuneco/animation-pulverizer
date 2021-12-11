29b70c55a07fc20fa683ea4820473d788d0c9530
yuneco <yukiyuneco@gmail.com>
2021-12-12 00:40:51 +0900

    デモをGitHubPages用にビルド

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -3,6 +3,10 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
